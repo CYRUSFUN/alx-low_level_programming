ed3f193b2c652fea1b8b480c7a484db8e570366f
CYRUSFUN <cyrustayo1@gmail.com>
2023-03-20 15:56:24 -0700

    task 8

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,14 +1,12 @@
-#include <stdio.h>
 #include <unistd.h>
+
 /**
- * main - 101-quote.c
- * description - Write a C program that prints exactly and that piece of
- * art is useful\"
- *  - Dora Korpar, 2015-10-19, followed by a new line, to the standard error.
- * Return: Always 0 (Success)
+ * main - prints "and that piece of art is useful" - Dora Korpar, 2015-10-19",
+ * followed by newline, to standard error.
+ * Return: Always 1.
  */
 int main(void)
 {
-	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
-	return (1);
+        write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+        return (1);
+}
