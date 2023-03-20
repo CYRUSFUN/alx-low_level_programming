50b9c9276e6725959f162a6a8ad2e9a94ada4f82
CYRUSFUN <cyrustayo1@gmail.com>
2023-03-20 15:18:44 -0700

    sd >

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,5 +1,5 @@
-#include <stdio.h>
-#include <unisted.h>
+#include<stdio.h>
+#include<unisted.h>
 /**
  * main - 101-quote.c
  * description - Write a C program that prints exactly and that piece of
@@ -9,8 +9,6 @@
  */
 int main(void)
 {
-	write(2,
-	      "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",
-	      59);
-	return (1);
+write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n",sizeof("and that piece of art is useful\" - Dora Korpar, 2015-10-19));
+return (1);
 }
